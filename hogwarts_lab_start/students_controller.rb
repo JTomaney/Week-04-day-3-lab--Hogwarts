2a8a8a77f1a78a4205d11d7805994f382cb051ff
Jonathan Tomaney <jon.tomaney@gmail.com>
2019-04-24 16:11:59 +0100

    Borked it

--- hogwarts_lab_start/students_controller.rb
+++ hogwarts_lab_start/students_controller.rb
@@ -5,15 +5,35 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
-
-# show
-
+get '/students' do
+  @students = Student.all()
+  erb(:index)
+end
 # new
-
+get '/students/new' do
+  @houses = House.all()
+  erb(:new)
+end
+# show
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:show)
+end
 # create
-
+post '/students' do
+  @student = Student.new(params)
+  @student.save()
+  redirect to '/students'
+end
 # edit
-
+get '/students/:id/edit' do
+  @houses = House.all()
+  @student = Student.find(params[:id])
+  erb(:edit)
+end
 # update
-
+post '/students/:id' do
+  Student.new(params).update()
+  redirect to '/students'
+end
 # destroy
